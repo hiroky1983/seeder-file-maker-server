739399815122ff4bd834b17641e80f79affd4fcd
hiroky1983 <hirockysan1983@gmail.com>
2023-12-24 01:07:00 +0900

    コメントアウトを戻す

--- src/domain/model/prompt/prompt.go
+++ src/domain/model/prompt/prompt.go
@@ -1,7 +1,9 @@
 package prompt
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 )
 
 func GeneratePromptText(prompt string) string {
@@ -23,17 +25,17 @@ func GeneratePromptText(prompt string) string {
 	return templateText
 }
 
-// func ValidatePromptText(prompt string) error {
-// 	// マイグレーションのパターンを定義
-// 	pattern := `(?i)CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?\w+\s*\([^\)]+\)`
+func ValidatePromptText(prompt string) error {
+	// マイグレーションのパターンを定義
+	pattern := `(?i)CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?\w+\s*\([^\)]+\)`
 
-// 	matched, err := regexp.MatchString(pattern, prompt)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if !matched {
-// 		err = errors.New("マイグレーションの形式が違います")
-// 		return err
-// 	}
-// 	return nil
-// }
+	matched, err := regexp.MatchString(pattern, prompt)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		err = errors.New("マイグレーションの形式が違います")
+		return err
+	}
+	return nil
+}
